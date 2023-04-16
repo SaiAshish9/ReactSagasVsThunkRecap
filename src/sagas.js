f702033516d3b06e2659b47fbe621c5ba51e9aeb
SaiAshish <saiashish3760@gmail.com>
2023-04-17 02:03:17 +0530

    Add files via upload

--- src/sagas.js
+++ src/sagas.js
@@ -1,14 +1,18 @@
-import { call, put, takeEvery } from 'redux-saga/effects'
+import { call, put, takeEvery, throttle } from 'redux-saga/effects'
 import { GET_USERS_FETCH, getUsersSuccess } from "./actions";
 
 function *workGetUsersFetch(){
     const response = yield call(fetch, 'https://jsonplaceholder.typicode.com/users');
     const users = yield response.json();
+    // const response1 = yield call(fetch, 'https://jsonplaceholder.typicode.com/profile');
+    // const response2 = yield call(fetch, 'https://jsonplaceholder.typicode.com/profile');
     yield put(getUsersSuccess(users));
 }
 
 function *mySaga(){
-   yield takeEvery(GET_USERS_FETCH, workGetUsersFetch);
+//  yield takeEvery(GET_USERS_FETCH, workGetUsersFetch);
+    yield throttle('2000', GET_USERS_FETCH, workGetUsersFetch);
+//  This will fetch after every 2 seconds
 }
 
 export default mySaga;
